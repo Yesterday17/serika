fb232e74236cf64522cc1bbab67ebea37aea5909
Yesterday17 <mmf@mmf.moe>
2023-02-21 12:06:51 +0800

    Set typing action

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use once_cell::sync::Lazy;
 use reqwest::Client;
 use serde_json::{json, Value};
-use teloxide::prelude::*;
+use teloxide::{prelude::*, types::ChatAction};
 
 static API_URL: Lazy<String> = Lazy::new(|| std::env::var("API_URL").unwrap());
 
@@ -25,8 +25,25 @@ async fn chat_handler(client: Arc<Client>, bot: Bot, message: Message) -> Respon
     let chat_id = message.chat.id;
     if let Some(text) = message.text() {
         if text.starts_with('/') {
+            let (command, arguments) = text[1..].split_once(' ').unwrap_or((&text[1..], ""));
+            match command {
+                "start" => {
+                    bot.send_message(chat_id, "Hello! I'm Serika, a chatbot.")
+                        .await?;
+                }
+                "session" => {
+                    // TODO: set to new session id
+                }
+                _ => {
+                    bot.send_message(chat_id, "Unknown command")
+                        .reply_to_message_id(message.id)
+                        .await?;
+                }
+            }
             return Ok(());
         }
+
+        bot.send_chat_action(chat_id, ChatAction::Typing).await?;
 
         let response = client
             .post(format!("{}/message/tg-{}-default", *API_URL, chat_id))
@@ -44,10 +61,6 @@ async fn chat_handler(client: Arc<Client>, bot: Bot, message: Message) -> Respon
         let data = response.json::<Value>().await.unwrap();
         let response_text = data["response"].as_str().unwrap();
         bot.send_message(chat_id, response_text).await?;
-    } else {
-        bot.send_message(chat_id, "[SERIKA ERROR] Empty chat message")
-            .reply_to_message_id(message.id)
-            .await?;
     }
 
     Ok(())
